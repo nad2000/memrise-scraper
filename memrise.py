cfdf93f421bec7d76f6d1b17fc815901530be821
Rad Cirskis <nad2000@gmail.com>
2016-05-15 03:23:05 +1200

    refactored to use generators instead of dumping all directly

--- memrise.py
+++ memrise.py
@@ -1,4 +1,5 @@
-DEFAULT_COURSE = "/course/977288/korean-grammar-in-use-11/"
+COURSE_URL = "/course/977288/korean-grammar-in-use-11/"
+CARD_COLUMNS = ("col_a", "col_b")
     
 import codecs, sys
 import requests
@@ -14,32 +15,46 @@ def get_soup(url):
     return soup
 
     
-def dump_course(url):
-    soup = get_soup(url)
+def levels(*, course_url : str):
+    """
+    :course_url:   course URL
+    """
+    soup = get_soup(course_url)
     #levels = soup.find(lambda tag: tag.name == "div" and "levels" in tag.attrs.get("class"))
     levels = soup.find_all("a", class_="level")
     
     for l in levels:
-        url = l.attrs.get("href")
-        dump_level(url)
+        yield l.attrs.get("href")
 
 
-def dump_level(url):
-    soup = get_soup(url)
+def cards(*, level_url : str):
+    """
+    :level_url:   level URL
+    """
+    soup = get_soup(level_url)
     
-    for l in soup.find_all(lambda tag: tag.has_attr("data-thing-id")):
+    for thing in soup.find_all(lambda tag: tag.has_attr("data-thing-id")):
         
         try:
-            cols = [l.find("div", class_=col_name).find("div", class_="text").text
-                for col_name in ["col_a", "col_b"]
-            ]
-            print('\t'.join(cols))
+            cols = (thing.find("div", class_=col_name).find("div", class_="text").text
+                for col_name in CARD_COLUMNS)
         except:
-            pass
-    
+            continue
+            
+        yield cols
 
+            
+def dump_course(*, course_url : str):
+    """
+    :course_url:   course URL
+    """
+    for level_url in levels(course_url=course_url):
+        print("***", level_url)
+        for card in cards(level_url=level_url):
+            print('\t'.join(card))
+        
 if __name__ == "__main__":
-    url = DEFAULT_COURSE if len(sys.argv) < 2 else sys.argv[1]
-    dump_course(url)
+    course_url = COURSE_URL if len(sys.argv) < 2 else sys.argv[1]
+    dump_course(course_url=course_url)
     
     
