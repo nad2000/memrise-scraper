b19cacc70c3f1e4e0c1b609332507a5bac59e566
Rad Cirskis <nad2000@gmail.com>
2016-08-13 20:49:34 +1200

    storing card data in TSV files

--- memrise2tts.py
+++ memrise2tts.py
@@ -15,15 +15,24 @@ def dump_tts(*, course_url : str):
     for level_url, title in course.levels:
         print("*** %s (%s)" % (title, level_url), flush=True)
         output_dir = os.path.join(course.name, title)
-        os.makedirs(output_dir, exist_ok=True)
+        if not no_audio:
+            os.makedirs(output_dir, exist_ok=True)
 
-        for word, *_ in course.cards(level_url=level_url):
-            file_name = os.path.join(output_dir, word) + ".mp3"
-            if not os.path.exists(file_name):
-                tts = gTTS(word, lang="ko")
-                tts.save(file_name)
+        with open(os.path.join(course.name, title) + ".csv", "w") as level_file:
+            for card in course.cards(level_url=level_url):
+                card = list(card)
+                word = card[0]
+                level_file.write('\t'.join(card))
+                level_file.write('\n')
+                file_name = os.path.join(output_dir, word) + ".mp3"
+                if not no_audio and not os.path.exists(file_name):
+                    tts = gTTS(word, lang="ko")
+                    tts.save(file_name)
 
 
 if __name__ == "__main__":
+    no_audio = ("--no-audio" in sys.argv)
+    if no_audio:
+        sys.argv.pop(sys.argv.index("--no-audio"))
     course_url = COURSE_URL if len(sys.argv) < 2 else sys.argv[1]
     dump_tts(course_url=course_url)
