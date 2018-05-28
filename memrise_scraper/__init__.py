fa549311cb7d091c302462bdc2afd7835608ae66
Rad Cirskis <nad2000@gmail.com>
2018-05-28 12:27:07 +1200

    added click and support for building package with prb

--- memrise_scraper/__init__.py
+++ memrise_scraper/__init__.py
@@ -1,7 +1,20 @@
 """Memrise flashcar scraping."""
-__version__ = "0.2.0"
+__version__ = "0.3.0"
 
 # flake8: noqa
+import click
 from .memrise import *
 from .memrise2tts import *
 
+@click.command()
+@click.option("--tts", is_flag=True, help="Get Google TTS generated sound files.")
+@click.option("--lang", default="ko", help="Source language (need to be given for TTS).")
+@click.option("--no-audio", is_flag=True, help="Skip donwloading audio.")
+@click.argument("url", default=COURSE_URL)
+def scrape(url, tts, lang, no_audio):
+    sys.stdout = codecs.getwriter("utf-8")(sys.stdout.detach())
+    if tts:
+        dump_tts(course_url=url, no_audio=no_audio)
+        pass
+    else:
+        dump_course(course_url=url)
